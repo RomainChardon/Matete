1d96312f4c3f9d7778655329f3845a19a6354272
mathbroche <mathis.broche.sio2020@gmail.com>
2022-01-04 11:05:36 +0100

    test

--- API/Connexion.php
+++ API/Connexion.php
@@ -23,17 +23,10 @@ class Connexion{
         
     /**
      * RecupererAnnonces
-     * Récupère les annonces ayant été postée les 30 derniers jours
+     * Récupère les annonces ayant été mise en ligne
      * @return void
      */
     function RecupererAnnonces(){
-        
-        $dateActuelle = new DateTime();
-        $intervalplus30jours = new DateInterval("P30D");
-
-        $dateActuelleFormate = $dateActuelle->format('Y-m-d H:i:s');
-
-        $datePlus30jours = $dateActuelle->sub($intervalplus30jours)->format('Y-m-d H:i:s');
 
         $requete = $this->bdd->prepare('SELECT * FROM annonce a INNER JOIN lieu l ON l.id = a.lieu_id INNER JOIN categorie c ON c.id = a.categorie_id INNER JOIN users u ON u.id = a.producteur_id');
         $requete->execute();
@@ -45,9 +38,10 @@ class Connexion{
             $c = new Categorie($row['id'],$row['libelle']); 
             $l = new Lieu($row['id'],$row['coo_x'],$row['coo_y'],$row['desc_lieu'],$row['nom']);
             $a = new Annonce($row['id'],$row['lieu_id'],$row['categorie_id'],$row['producteur_id'],$row['creneaux_debut'],$row['creneaux_fin'],$row['libelle_produit'],$row['prix_unitaire'],$row['quantite'],$row['status'],$row['date_mise_en_ligne'],$l,$c,$p);
-            var_dump($a);
-            echo('<br><br>');
-            array_push($annoncesPOO,$a);
+            if ($row['status'] == "EnLigne"){
+                array_push($annoncesPOO,$a);
+            }
+            
         }
         return $annoncesPOO;
     }
