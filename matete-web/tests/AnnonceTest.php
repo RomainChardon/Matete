9a30d1df04ce8ed52946917ffe89479618dbe500
mathbroche <mathis.broche.sio2020@gmail.com>
2022-03-15 14:35:46 +0100

    Etape intégration continue

---
+++ matete-web/tests/AnnonceTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Tests;
+
+use App\Entity\Annonce;
+use PHPUnit\Framework\TestCase;
+
+class AnnonceTest extends TestCase
+{
+    public function testAssertTrue():void
+    {
+        $annonce = new  Annonce();
+        $date = new \DateTime();
+        $annonce->setLibelleProduit("oui");
+        $annonce->setDateMiseEnLigne($date);
+
+        $this->assertTrue($annonce->getDateMiseEnLigne()===$date);
+        $this->assertTrue($annonce->getLibelleProduit()=="oui");
+    }
+
+    public function testAssertFalse():void
+    {
+        $annonce = new  Annonce();
+        $date = new \DateTime();
+
+        $this->assertFalse($annonce->getDateMiseEnLigne()===$date);
+        $this->assertFalse($annonce->getLibelleProduit()=="oui");
+    }
+
+    public function testAssertEmpty():void
+    {
+        $annonce = new  Annonce();
+        $date = new \DateTime();
+
+        $this->assertEmpty($annonce->getDateMiseEnLigne()===$date);
+        $this->assertEmpty($annonce->getLibelleProduit()=="oui");
+    }
+}
